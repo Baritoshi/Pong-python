c223b1722c7443db542bff190fd70282dacf6399
Baritoshi <bartosz.f@live.com>
2022-04-12 19:33:54 +0200

    Initial commit

---
+++ Pong/Pong/Pong.py
@@ -0,0 +1,30 @@
+
+#Pong in Python
+#executed by Baritoshi
+#based on Simple Pong in Python 3 by @TokyoEdTech
+#1. Getting Started
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Baritoshi")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Paddle A
+paddle_a = turtle.Turtle
+
+
+#Paddle B
+
+
+#Ball
+
+
+
+
+#main game loop
+
+while True:
+    wn.update()
